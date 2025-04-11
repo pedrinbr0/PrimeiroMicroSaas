711887e6ca66dadc6bdfa1e9a299a8a434e795b4
pedro <paulosilvaotaviano@gmail.com>
2025-04-11 16:48:58 -0300

    Validação no model  produto

--- app/models/produto.rb
+++ app/models/produto.rb
@@ -1,2 +1,5 @@
 class Produto < ApplicationRecord
+  validates :nome, presence: true
+  validates :preco, presence: true, numericality: { greater_than: 0 }
+  validates :codigo_barra, presence: true, uniqueness: true
 end
